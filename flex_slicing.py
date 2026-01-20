05c832052cbe9a990032c81a70672c4a9e5b0d27
Gustavo Kanaiama <gukanaiama@usp.br>
2026-01-20 00:23:46 -0300

    starting new altprint

---
+++ flex_slicing.py
@@ -0,0 +1,10 @@
+from Altprint.core.flex_continuous_copy import FlexProcess, FlexPrint
+
+
+process = FlexProcess(settings_file="config/parameters/dev_flex_parameters.yml")
+part = FlexPrint(process)
+
+
+part.slice()
+part.make_layers()
+part.export_gcode("out/gcode/sliced_geometry.gcode")
